f8943165e948c575a2102a32cdeb323a03ad7f01
Selena X <selenaxx20@gmail.com>
2025-06-19 20:43:47 -0700

    init setup

---
+++ weather-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.js';
+
+// locate root for react to render app at
+const el = document.getElementById('root');
+
+// create React root to control rendering into the DOM elemen
+const root = ReactDOM.createRoot(el);
+
+// injects react component tree into DOM
+root.render(<App />);
