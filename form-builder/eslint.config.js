996ce71ff36f64f8548fa81870bd11c70be7f204
Selena X <selenaxx20@gmail.com>
2025-06-27 22:10:25 -0700

    chore: disable react-in-jsx-scope rule in ESLint config

--- form-builder/eslint.config.js
+++ form-builder/eslint.config.js
@@ -6,33 +6,34 @@ import globals from 'globals';
 import prettier from 'eslint-config-prettier';
 
 export default [
-	js.configs.recommended,
-	{
-		files: ['**/*.{js,jsx}'],
-		languageOptions: {
-			ecmaVersion: 'latest',
-			sourceType: 'module',
-			globals: globals.browser,
-			parserOptions: {
-				ecmaFeatures: {
-					jsx: true,
-				},
-			},
-		},
-		plugins: {
-			react,
-			'react-hooks': reactHooks,
-		},
-		rules: {
-			...react.configs.recommended.rules,
-			...reactHooks.configs.recommended.rules,
-			'spaced-comment': ['error', 'always'],
-		},
-		settings: {
-			react: {
-				version: 'detect',
-			},
-		},
-	},
-	prettier,
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'spaced-comment': ['error', 'always'],
+      'react/react-in-jsx-scope': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  prettier,
 ];
