a2cc0260facd7f67a6db53ff8c7425bcb1905bdf
Selena X <selenaxx20@gmail.com>
2025-06-28 21:34:56 -0700

    style: assign 2/3 and 1/3 width to FormEditor and Sidebar layout

--- form-builder/src/App.jsx
+++ form-builder/src/App.jsx
@@ -4,9 +4,13 @@ function App() {
   return (
     <div>
       <div className="flex justify-center p-10">
-        <div className="w-full max-w-5xl flex">
-          <Sidebar />
+        <div className="w-full max-w-5xl flex gap-4">
+          <div className="w-1/3">
+            <Sidebar />
+          </div>
+              <div className="w-2/3">
           <FormEditor />
+          </div>
         </div>
       </div>
     </div>
