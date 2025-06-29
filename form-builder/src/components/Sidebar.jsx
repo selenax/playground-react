8af1f9fdb629b00bbe79d9afabe40d8eb76d8353
Selena X <selenaxx20@gmail.com>
2025-06-28 19:55:08 -0700

    Added grid and fields option buttons

--- form-builder/src/components/Sidebar.jsx
+++ form-builder/src/components/Sidebar.jsx
@@ -1,3 +1,16 @@
-function Sidebar() {}
+function Sidebar() {
+  return (
+    <div>
+      <p className="text-gray-600">Select Fields</p>
+      <div className="grid grid-cols-2 gap-4">
+        <button>Text</button>
+        <button>Paragraph</button>
+
+        <button>Checkbox</button>
+        <button>Select</button>
+      </div>
+    </div>
+  );
+}
 
 export default Sidebar;
