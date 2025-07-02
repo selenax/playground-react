3a12ae353ab353a670692b0214673c71d0921faf
Selena X <selenaxx20@gmail.com>
2025-07-01 22:19:45 -0700

    connected Sidebar buttons to field creation handler

--- form-builder/src/components/Sidebar.jsx
+++ form-builder/src/components/Sidebar.jsx
@@ -1,14 +1,20 @@
-function Sidebar() {
+function Sidebar({ onAddField }) {
   return (
     <div>
       <p className="text-gray-600">Select Fields</p>
       <div className="grid grid-cols-2 gap-4">
-        <button onClick={() => console.log('testing')} className="btn-primary">
+        <button onClick={() => onAddField('text')} className="btn-primary">
           Text
         </button>
-        <button className="btn-primary">Paragraph</button>
-        <button className="btn-primary">Checkbox</button>
-        <button className="btn-primary">Select</button>
+        <button onClick={() => onAddField('paragraph')} className="btn-primary">
+          Paragraph
+        </button>
+        <button onClick={() => onAddField('checkbox')} className="btn-primary">
+          Checkbox
+        </button>
+        <button onClick={() => onAddField('select')} className="btn-primary">
+          Select
+        </button>
       </div>
     </div>
   );
