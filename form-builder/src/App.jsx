fc3b09d2be4d817857496670cbe8ada0fbf318b2
Selena X <selenaxx20@gmail.com>
2025-07-01 21:47:49 -0700

    feat: add field creation logic and state

--- form-builder/src/App.jsx
+++ form-builder/src/App.jsx
@@ -1,15 +1,33 @@
+import { useState } from 'react';
 import Sidebar from './components/Sidebar';
 import FormEditor from './components/FormEditor';
+
 function App() {
+  const [collection, setCollection] = useState([]);
+
+  // handler: create and add a new form field to collection
+  const handleClickField = (type) => {
+    // form schema
+    const newField = {
+      id: crypto.randomUUID(), // unique identifier for field
+      type, // field type: 'text','checkbox','paragraph','select'
+      label: 'Untitled', // default label
+      required: false, // default required status
+    };
+    // adds to collection
+    setCollection((prev) => [...prev, newField]);
+    console.log('Added:', newField);
+  };
+
   return (
     <div>
       <div className="flex justify-center p-10">
         <div className="w-full max-w-5xl flex gap-4">
           <div className="w-1/3">
-            <Sidebar />
+            <Sidebar onClick={handleClickField} />
           </div>
-              <div className="w-2/3">
-          <FormEditor />
+          <div className="w-2/3">
+            <FormEditor />
           </div>
         </div>
       </div>
