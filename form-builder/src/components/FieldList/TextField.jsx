5d07e81123433fbd925f43cb0ed014da06025f5d
Selena X <selenaxx20@gmail.com>
2025-07-03 22:38:14 -0700

    feat: added input logic in TextField

--- form-builder/src/components/FieldList/TextField.jsx
+++ form-builder/src/components/FieldList/TextField.jsx
@@ -1,5 +1,15 @@
 const TextField = () => {
-  return <div></div>;
+  return (
+    <div>
+      <input
+        type="text"
+        // className="w-full border rounded px-2 py-1"
+        className="btn-primary w-full"
+        placeholder="Enter input"
+        
+      />
+    </div>
+  );
 };
 
 export default TextField;
