3527f293df7bea59ce0b9dd7ec25227dbb9e7d79
Selena X <selenaxx20@gmail.com>
2025-07-03 21:51:25 -0700

    feat: render field components dynamically based on collection

--- form-builder/src/components/FormEditor.jsx
+++ form-builder/src/components/FormEditor.jsx
@@ -1,3 +1,28 @@
-function FormEditor () {}
+import TextField from './FieldList/TextField';
+import CheckboxField from './FieldList/CheckboxField';
+import ParagraphField from './FieldList/ParagraphField';
+import SelectField from './FieldList/SelectField';
+
+const FormEditor = ({ collection }) => {
+  return (
+    <div>
+      {collection.map((field) => {
+        switch (field.type) {
+          case 'text':
+            return <TextField key={field.id} field={field} />;
+          case 'checkbox':
+            return <CheckboxField key={field.id} field={field} />;
+          case 'paragraph':
+            return <ParagraphField key={field.id} field={field} />;
+          case 'select':
+            return <SelectField key={field.id} field={field} />;
+          default:
+            return null;
+        }
+      })}
+      ;
+    </div>
+  );
+};
 
 export default FormEditor;
